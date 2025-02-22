de4c5344e06e136344e3a52e5512794c57e3c59c
vanilson muhongo <vanilsonmuhongo@gmail.com>
2025-02-22 18:39:28 +0000

    feat: Configuration kafka stream Producer.
    -- Added bootstrap configuration to connect to kafka cluster
    -- Created method to send a message from producer with keys and without keys.

---
+++ course-kafka-event/src/main/java/code/with/vanilson/config/ProducerConfig.java
@@ -0,0 +1,35 @@
+package code.with.vanilson.config;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+import static code.with.vanilson.util.PropertyUtil.*;
+
+/**
+ * ProducerConfig
+ *
+ * @author vamuhong
+ * @version 1.0
+ * @since 2025-02-22
+ */
+public class ProducerConfig {
+
+    private ProducerConfig() {
+        throw new AssertionError("This class cannot be instantiated");
+    }
+
+    public static Properties getKafkaProperties() {
+        Properties props = new Properties();
+        props.setProperty(BOOTSTRAP_SERVERS, HOSTNAME);
+        props.setProperty(ACKS, "all");
+        props.setProperty(RETRIES, Integer.toString(Integer.MAX_VALUE));
+        props.setProperty(PRODUCER_KEY_SERIALIZER, StringSerializer.class.getName());
+        props.setProperty(PRODUCER_VALUE_SERIALIZER, StringSerializer.class.getName());
+        props.setProperty("linger.ms.config", "3");
+        props.setProperty("compression.type", "snappy");
+        props.setProperty("batch.size", Integer.toString(32 * 1024));
+        return props;
+    }
+
+}
---
+++ course-kafka-event/src/test/java/code/with/vanilson/KafkaEventStreamAppTest.java
@@ -0,0 +1,16 @@
+package code.with.vanilson;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple KafkaEventStreamApp.
+ */
+class KafkaEventStreamAppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
